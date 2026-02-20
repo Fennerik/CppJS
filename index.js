652e99ea26c014cb718f085a2ab3c81f771f469b
Fennerik <fennerikpi@outlook.de>
2026-02-20 21:19:36 +0100

    create inital code

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("This little program demonstrates how to integrate C++ in JavaScript")
+let C = 0 // Create variable
+console.log(C) // Print variable for the first time
+C++ // Use C++ in Javascript
+console.log(C) // Print variable again to show the usage of C++ in JavaScript
+console.log("Don't get it? Look at the source code.")
